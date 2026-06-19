b71f8272e64c12cab4f0274c0f421e031d5f08e1
saim ali <saim.aliam2233@gmail.com>
2026-06-19 12:54:34 -0400

    Create api.php
    a

---
+++ api.php
@@ -0,0 +1,22 @@
+<?php
+
+function respond($data, $code = 200) {
+    http_response_code($code);
+    header('Content-Type: application/json');
+    echo json_encode($data);
+    exit;
+}
+
+$action = $_GET['action'] ?? '';
+
+if ($action === "quiz") {
+    require "/var/www/private_data/validator/evaluate_quiz.php";
+    exit;
+}
+
+if ($action === "lab") {
+    require "/var/www/private_data/validator/evaluate_lab.php";
+    exit;
+}
+
+respond(["error" => "Invalid action"], 400);
